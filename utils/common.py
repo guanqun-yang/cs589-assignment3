e9a3a6065828a4d1c7fde50de13688cfcb52a55b
Guanqun Yang <guanqun.yang@outlook.com>
2022-10-31 02:10:22 -0400

    update

---
+++ utils/common.py
@@ -0,0 +1,26 @@
+import os
+import shutil
+import hashlib
+
+def sha256sum(filename):
+    h  = hashlib.sha256()
+    b  = bytearray(128*1024)
+    mv = memoryview(b)
+    with open(filename, 'rb', buffering=0) as f:
+        for n in iter(lambda : f.readinto(mv), 0):
+            h.update(mv[:n])
+    return h.hexdigest()
+
+def post_processing():    
+    # hashing each data file
+    l = list()
+    for filename in os.listdir(save_path):
+        l.append({
+            "name": filename,
+            "sha256": sha256sum(save_path / filename)
+        })
+
+    pd.DataFrame(l).to_pickle(save_path / "fingerprint.pkl")
+
+    # zipping submission/ directory
+    shutil.make_archive("submission", "zip", save_path)
